0206b851d80e292bb96c5ea0e7e133ce59bd92d2
OasesOng <fish.0829@outlook.com>
2016-03-02 17:50:41 +0800

    first commit

---
+++ StockBoy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StockBoy.Startup))]
+namespace StockBoy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
